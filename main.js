9c53eb7f91a92129a12913725fe14171a56b00b6
Aline Dobo <dobo.aline@gmail.com>
2026-05-19 13:06:31 +0200

    pre-daily

--- main.js
+++ main.js
@@ -7,47 +7,54 @@ function renderBookListing() {
 
     for (let i = 0; i < books.length; i++) {
         bookListingRef.innerHTML += /*html*/ `
-            <div class="book-preview" onclick="showBookDetails(${i})">
-                <img src="./assets/images/Vectorizeimages-Iconpack-Book.512.png" alt="icon of a book">
+            <div class="book-preview">
+                <img src="./assets/images/Vectorizeimages-Iconpack-Book.512.png" alt="icon of a book" onclick="showBookDetails(${i})">
                 <p>${books[i].name}</p>
             </div>
         `;
     }
 }
 
-function showBookDetails(index) {
-    dialofRef.showModal();
 
-    dialofRef.innerHTML = /*html*/ `
+
+
+function showBookDetails(index) {
+    const dialogRef = document.getElementById("book-dialog");
+
+    dialogRef.showModal();
+
+    dialogRef.innerHTML = /*html*/ `
     <div id="dialog-container">
         <header>
-            <h2 id="title">${books[i].name}</h2>
+            <h2 id="title">${books[index].name}</h2>
             <img src="./assets/icons/close-button.svg" alt="close button" />
         </header>
         <section>
             <div id="book-details">
                 <div id="book-numbers">
-                    <h2 id="price"></h2>
-                    <div>
+                    <h2 id="price">${books[index].price}€</h2>
+                    <div id="likes">
                         <p id="likes-counter">1212</p>
-                        <img
-                        src="./assets/icons/Github-Octicons-Heart-16.512.png"
-                        alt="heart icon"
-                        />
+                        <button>
+                            <img
+                            src="./assets/icons/Github-Octicons-Heart-16.512.png"
+                            alt="heart icon"
+                            />
+                        </button>
                     </div>
                 </div>
                 <table>
                     <tr>
                         <th>Author:</th>
-                        <td>${books[i].author}</td>
+                        <td>${books[index].author}</td>
                     </tr>
                     <tr>
                         <th>Published:</th>
-                        <td>${books[i].publishedYear}</td>
+                        <td>${books[index].publishedYear}</td>
                     </tr>
                     <tr>
                         <th>Genre:</th>
-                        <td>${books[i].genre}</td>
+                        <td>${books[index].genre}</td>
                     </tr>
                 </table>
             </div>
